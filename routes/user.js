9f832c12ad0f0640e652e78205cb924b1f7e6a97
merttdemirr <demir_14@windowslive.com>
2026-02-09 18:04:34 +0300

    düzeltme

--- routes/user.js
+++ routes/user.js
@@ -13,8 +13,6 @@ router.use("/blog",controllersUser.blog );
 
 router.use("/amac",controllersUser.amac );
 
-router.use("/komisyon",controllersUser.online)
-
 router.use("/faaliyet",controllersUser.faaliyet)
 
 
