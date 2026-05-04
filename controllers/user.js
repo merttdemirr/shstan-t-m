c17b3fb5544be03fa56a8825d81d992adb6074ed
merttdemirr <demir_14@windowslive.com>
2026-05-04 10:53:30 +0300

    mail güncelleme

--- controllers/user.js
+++ controllers/user.js
@@ -1,84 +1,97 @@
-const express=require("express")
-const router=express.Router()
-const path=require("path")
-const {Op}=require("sequelize")
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const { Op } = require("sequelize");
 const nodemailer = require("nodemailer");
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.about=function(req,res){
-    res.render(path.join(__dirname,"../views/users","about2"))
-}
-
-
-exports.contact=function(req,res){
-    res.render(path.join(__dirname,"../views/users","komisyonlarımız"))
-}
-
-exports.blog=function (req, res) {
-    db.query("select * from blog")
-        .then(result=>{
-            res.render("users/blog",{
-                title:"Bloglar",
-                blogs:result[0],
-               
-            });
-        })
-        .catch(err=>console.log(err))
-}
-
-exports.faaliyet=function(req,res){
-    res.render(path.join(__dirname,"../views/users","faaliyet"))
-}
-
-exports.amac=function(req,res){
-    res.render(path.join(__dirname,"../views/users","amac"))
-}
-
-exports.üyelik=function(req,res){
-    res.render(path.join(__dirname,"../views/users","üyelik"))
-}
-// KOMİSYONLAR
-exports.komisyon=function(req,res){
-    res.render(path.join(__dirname,"../views/users","komisyonlarımız"))
-}
-
-exports.engbakanasayfa=function(req,res){
-    res.render(path.join(__dirname,"../views/users/komisyonlar/engellibakicilar/engellibakicilaranasayfa.ejs"))
-}
-
-exports.asdepanasayfa=function(req,res){
-    res.render(path.join(__dirname,"../views/users/komisyonlar/asdep/asdepanasayfa.ejs"))
-}
-// temsilci
-exports.temsilci_basvuru=function(req,res){
-    res.render(path.join(__dirname,"../views/users/temsilci","basvuru"))
-}
+// ===================== SMTP (Brevo) =====================
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "aa1e9b001@smtp-brevo.com",   // Brevo hesabın
+    pass: "xsmtpsib-965e7b458d1441f64bd86722bb65c4cc94bac57795a94d25dbf7e96a4477b122-dJPGwyC3oR36mwRf"            // 🔥 SADECE SMTP KEY (xkeysib DEĞİL)
+  }
+});
 
 
+// ===================== SAYFALAR =====================
 
-// Başvuru gönderim kısmıı
+exports.about = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "about2"));
+};
+
+exports.contact = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "komisyonlarımız"));
+};
+
+exports.blog = (req, res) => {
+  db.query("select * from blog")
+    .then(result => {
+      res.render("users/blog", {
+        title: "Bloglar",
+        blogs: result[0],
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.faaliyet = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "faaliyet"));
+};
+
+exports.amac = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "amac"));
+};
+
+exports.üyelik = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "üyelik"));
+};
+
+exports.komisyon = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "komisyonlarımız"));
+};
+
+exports.engbakanasayfa = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/komisyonlar/engellibakicilar/engellibakicilaranasayfa.ejs"));
+};
+
+exports.asdepanasayfa = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/komisyonlar/asdep/asdepanasayfa.ejs"));
+};
+
+exports.anasayfa = (req, res) => {
+  res.render(path.join(__dirname, "../views/users", "anasayfa"));
+};
+
+
+// ===================== TEMSİLCİ BAŞVURU =====================
+
+exports.temsilci_basvuru = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/temsilci", "basvuru"));
+};
+
 exports.temsilci_basvuru_post = async (req, res) => {
-    const { adsoyad, telefon, il, unvan, kurum, aciklama } = req.body;
+  const { adsoyad, telefon, il, unvan, kurum, aciklama } = req.body;
 
-    const msg = {
-        to: "sosyalhizmetsen@gmail.com",
-        from: "ashbkomisyon3@gmail.com",
-        subject: "Yeni İş Yeri Temsilcisi Başvurusu",
-        html: `
-            <h3>Yeni Başvuru</h3>
-            <p><b>Ad Soyad:</b> ${adsoyad}</p>
-            <p><b>Telefon:</b> ${telefon}</p>
-            <p><b>İl:</b> ${il}</p>
-            <p><b>Unvan:</b> ${unvan}</p>
-            <p><b>Kurum:</b> ${kurum}</p>
-            <p><b>Açıklama:</b> ${aciklama}</p>
-        `
-    };
-
+  const msg = {
+    to: "sosyalhizmetsen@gmail.com",
+    from: "ashbkomisyon3@gmail.com",
+    subject: "Yeni İş Yeri Temsilcisi Başvurusu",
+    html: `
+      <h3>Yeni Başvuru</h3>
+      <p><b>Ad Soyad:</b> ${adsoyad}</p>
+      <p><b>Telefon:</b> ${telefon}</p>
+      <p><b>İl:</b> ${il}</p>
+      <p><b>Unvan:</b> ${unvan}</p>
+      <p><b>Kurum:</b> ${kurum}</p>
+      <p><b>Açıklama:</b> ${aciklama}</p>
+    `
+  };
 
   try {
-    await sgMail.send(msg);
+    await transporter.sendMail(msg);
     res.redirect("/temsilci/basvurualindi");
   } catch (error) {
     console.log(error);
@@ -86,60 +99,56 @@ exports.temsilci_basvuru_post = async (req, res) => {
   }
 };
 
-exports.temsilci_basvuru_alındı=function(req,res){
-    res.render(path.join(__dirname,"../views/users/temsilci","basvurualındı"))
-}
+exports.temsilci_basvuru_alındı = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/temsilci", "basvurualındı"));
+};
 
 
+// ===================== FAALİYETLER =====================
 
-// faaliyetler
+exports.faaliyet_asdep_ek_ödeme = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/faaliyetler", "1asdepeködeme"));
+};
 
-exports.faaliyet_asdep_ek_ödeme=function(req,res){
-    res.render(path.join(__dirname,"../views/users/faaliyetler","1asdepeködeme"))
-}
+exports.faaliyet_huzurevi_ek_ödeme = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/faaliyetler", "2huzurevieködeme"));
+};
 
-exports.faaliyet_huzurevi_ek_ödeme=function(req,res){
-    res.render(path.join(__dirname,"../views/users/faaliyetler","2huzurevieködeme"))
-}
+exports.faaliyet_asdep_izin = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/faaliyetler", "3asdepizin"));
+};
 
-exports.faaliyet_asdep_izin=function(req,res){
-    res.render(path.join(__dirname,"../views/users/faaliyetler","3asdepizin"))
-}
+exports.faaliyet_risk_aile = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/faaliyetler", "4riskAile"));
+};
 
-exports.faaliyet_risk_aile=function(req,res){
-    res.render(path.join(__dirname,"../views/users/faaliyetler","4riskAile"))
-}
 
-exports.anasayfa=function(req,res){
-     res.render(path.join(__dirname,"../views/users","anasayfa"))
-}
+// ===================== ONLINE ÜYELİK =====================
 
-//online üyelik
-exports.onlineüyelik=function(req,res){
-    res.render(path.join(__dirname,"../views/users/online","onlineüyelik"))
-}
+exports.onlineüyelik = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/online", "onlineüyelik"));
+};
 
 exports.üyelik_basvuru_post = async (req, res) => {
-    const { adsoyad, telefon, il, unvan, gorev_kurum, aciklama } = req.body;
+  const { adsoyad, telefon, il, unvan, gorev_kurum, aciklama } = req.body;
 
-    const msg = {
-        to: "sosyalhizmetsen@gmail.com",
-        from: "ashbkomisyon3@gmail.com",
-        subject: " Başvurusu",
-        html: `
-            <h3>Yeni Başvuru</h3>
-            <p><b>Ad Soyad:</b> ${adsoyad}</p>
-            <p><b>Telefon:</b> ${telefon}</p>
-            <p><b>İl:</b> ${il}</p>
-            <p><b>Unvan:</b> ${unvan}</p>
-            <p><b>Kurum:</b> ${gorev_kurum}</p>
-            <p><b>Açıklama:</b> ${aciklama}</p>
-        `
-    };
-
+  const msg = {
+    to: "sosyalhizmetsen@gmail.com",
+    from: "aa1e9b001@smtp-brevo.com",
+    subject: "Yeni Üyelik Başvurusu",
+    html: `
+      <h3>Yeni Başvuru</h3>
+      <p><b>Ad Soyad:</b> ${adsoyad}</p>
+      <p><b>Telefon:</b> ${telefon}</p>
+      <p><b>İl:</b> ${il}</p>
+      <p><b>Unvan:</b> ${unvan}</p>
+      <p><b>Kurum:</b> ${gorev_kurum}</p>
+      <p><b>Açıklama:</b> ${aciklama}</p>
+    `
+  };
 
   try {
-    await sgMail.send(msg);
+    await transporter.sendMail(msg);
     res.redirect("/onlineuyelikalindi");
   } catch (error) {
     console.log(error);
@@ -147,13 +156,10 @@ exports.üyelik_basvuru_post = async (req, res) => {
   }
 };
 
-exports.üyelik_basvuru_alındı=function(req,res){
-    res.render(path.join(__dirname,"../views/users/online","üyelikbasvurualındı"))
-}
+exports.üyelik_basvuru_alındı = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/online", "üyelikbasvurualındı"));
+};
 
-
-
-
-exports.kvkk=function(req,res){
-    res.render(path.join(__dirname,"../views/users/online","kvkk"))
-}
+exports.kvkk = (req, res) => {
+  res.render(path.join(__dirname, "../views/users/online", "kvkk"));
+};
